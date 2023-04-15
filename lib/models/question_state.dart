5e879991893f50cdb1b76c0c60724e8ee28c0195
KobayashiYoh <y.kobayashi@4nonome.com>
2023-04-15 17:29:59 +0900

    QuestionStateクラスを作成

---
+++ lib/models/question_state.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'question_state.freezed.dart';
+
+@freezed
+class QuestionState with _$QuestionState {
+  const factory QuestionState({
+    required int questionIndex,
+  }) = _QuestionState;
+}
